b4f171418502082761053f844e00716ac44e35bb
jazz5647 <130833738+jazz5647@users.noreply.github.com>
2024-11-24 15:32:36 +0300

    DESTROY UR OBJECT!!!!

---
+++ Destroying.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroying : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //UNICHTOZHENIYE IGROVOGO OBYEKTA
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
