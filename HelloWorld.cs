6526bb8f1fd56e6b6c2ad842d0b0cb2010495164
jazz5647 <130833738+jazz5647@users.noreply.github.com>
2024-11-24 15:16:59 +0300

    Uploadin' just some test script.

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Хэллоу Ворлд выводимъ в консоль
+        print("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
