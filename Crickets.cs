e7784bede04624dbc08bf24e4606bf9b53180acb
jazz5647 <130833738+jazz5647@users.noreply.github.com>
2024-12-08 14:45:35 +0300

    Crickets
    Crickets

--- Crickets.cs
+++ Crickets.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cricket : MonoBehaviour
+public class Crickets : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
